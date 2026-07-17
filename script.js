24d5a7a1484c96d72b841142dff0d184c9bfc922
Arkhavy <arkhavy@gmail.com>
2026-07-17 10:23:00 +0200

    [CORE] Add first and second point of project
    Addition of some comment dividers for clarity
    Addition of ID attribute for Book constructor
    Addition of myLibrary array
    	Will contain Book objects
    Addition of function addBookToLibrary

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const myLibrary = [];
+
+/* ************************************************************************** */
+/*                              Book CONSTRUCTOR                              */
+/* ************************************************************************** */
 function Book(title, author, pageCount, isRead) {
 	if (!new.target) {
 		throw Error("You must use the 'new' operator to call the constructor");
 	}
 
+	/* ******************************* ATTRIBUTES ******************************* */
 	this.title = title; // String
 	this.author = author; // String
 	this.pageCount = pageCount; // Number
 	this.isRead = isRead; // Boolean
+	this.id = crypto.randomUUID(); // Unique UUID
+
+	/* ********************************* METHODS ******************************** */
 	this.info = function () {
 		if (this.isRead) {
 			return (`${this.title} by ${this.author}, ${this.pageCount} pages, has been read.`);
@@ -15,7 +24,15 @@ function Book(title, author, pageCount, isRead) {
 	}
 }
 
-theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+function addBookToLibrary(title, author, pageCount, isRead) {
+	const book = new Book(title, author, pageCount, isRead);
+	myLibrary.push(book);
+}
+
+/* ************************************************************************** */
+/*                                    TESTS                                   */
+/* ************************************************************************** */
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 console.log(theHobbit.info());
 console.log(theHobbit);
 
