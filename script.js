f96ab01be1af7d288f0a0993f812dd755ff8ab8e
Arkhavy <arkhavy@gmail.com>
2026-07-19 11:30:03 +0200

    [CORE] 4th point on the way
    Ongoing function createForm when clicking the "New Book" button
    Includes createLabel, createInput and createButton helper functions
    	Mostly to avoid repetition since data is the only thing changing
    	Might end up being refactored at some point
    
    ToDo :
    	Gather submitted data to create a book into a new card (with ID)
    	Submitting a form shouldn't reset the page (event.preventDefault ?)
    	Might need to add info for the forms inputs (name, required, size ?)
    	Multiple forms are possible, make it work because why not :D
    	A bit more design for the form display (spacing and borders mostly)

--- script.js
+++ script.js
@@ -28,6 +28,60 @@ function addBookToLibrary(title, author, pageCount, isRead) {
 }
 
 /* ************************************************************************** */
+/*                          BUTTON AND FORM HANDLING                          */
+/* ************************************************************************** */
+const bookForm = document.getElementById("bookForm");
+const bookFormButton = document.getElementById("bookFormButton");
+
+function createLabel(text, id) {
+	const label = document.createElement("label");
+	label.textContent = text;
+	label.htmlFor = id;
+	return (label);
+}
+
+function createInput(id, type) {
+	const input = document.createElement("input");
+	input.id = id;
+	input.type = type;
+	return (input);
+}
+
+function createButton(text, type) {
+	const button = document.createElement("button");
+	button.textContent = text;
+	button.type = type;
+	button.formTarget = "_self";
+	return (button);
+}
+
+function createForm() {
+	const newForm = document.createElement("form");
+
+	/* ********************************** TITLE ********************************* */
+	newForm.appendChild(createLabel("Book title:", "title"));
+	newForm.appendChild(createInput("title", "text"))
+
+	/* ********************************* AUTHOR ********************************* */
+	newForm.appendChild(createLabel("Book author:", "author"));
+	newForm.appendChild(createInput("author", "text"));
+
+	/* ******************************* PAGE COUNT ******************************* */
+	newForm.appendChild(createLabel("Book page count:", "pageCount"));
+	newForm.appendChild(createInput("pageCount", "tel"));
+
+	/* ********************************* IS READ ******************************** */
+	newForm.appendChild(createLabel("Book read status:", "isRead"));
+	newForm.appendChild(createInput("isRead", "text"));
+	newForm.appendChild(createButton("Submit", "submit"));
+	return (newForm);
+}
+
+bookFormButton.addEventListener("click", () => {
+	bookForm.appendChild(createForm());
+});
+
+/* ************************************************************************** */
 /*                                    TESTS                                   */
 /* ************************************************************************** */
 function displayLibrary() {
